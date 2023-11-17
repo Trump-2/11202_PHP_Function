25181f2ee7356ec0b60cd5a66370b6574befe0d7
YIHONG <akashidaiki2@gmail.com>
2023-11-17 11:00:13 +0800

    全域變數在 function 內使用的方式

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 // 自訂函數
+
+// 全域變數 $c
+$c = 100;
 function sum($a, $b)
 {
-  $sum = $a + $b;
+  // 重要：要在 function 內使用全域變數就要這樣寫
+  global $c;
+  $sum = $a + $b + $c;
   echo "輸入：" . $a . "、" . $b;
   echo "<br>";
   return $sum;
@@ -14,3 +19,7 @@ echo "<hr>";
 
 $total = sum(37, 15);
 echo "總和是：$total";
+echo "<hr>";
+
+// 函數本身也是變數的證明
+echo "總和是：" . sum(29, 52);
