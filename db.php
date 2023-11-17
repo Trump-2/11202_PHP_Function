c9242732afd7f046b6a85e1a67a7dab1f13dd82b
YIHONG <akashidaiki2@gmail.com>
2023-11-17 16:36:43 +0800

    增加條件參數來提高函式的功能性

--- db.php
+++ db.php
@@ -1,16 +1,27 @@
 <?php
 
-$rows = all('member', 'users');
+$rows = all('school', 'students', ' where `dept` = 1');
 printdata($rows);
 
-function all($dbname = null, $tableName = null)
+function all($dbname = null, $tableName = null, $where = '')
 {
   $dsn = "mysql:host=localhost;charset=utf8;dbname=$dbname";
   $pdo = new PDO($dsn, 'root', '');
+  $sel = "select * from `$tableName` ";
 
-  if (isset($tableName) && isset($dbname) && !empty($table) && !empty($dbname)) {
-
-    $sql = "select * from `$tableName`";
+  if (isset($tableName) && isset($dbname) && !empty($tableName) && !empty($dbname)) {
+    if (is_array($where)) {
+      if (!empty($where)) {
+        foreach ($where as $col => $value) {
+          $tmp[] = "`$col`='$value'";
+        }
+        $sql = "$sel where " . join(" && ", $tmp);
+      } else {
+        $sql = $sel;
+      }
+    } else {
+      $sql = "$sel $where";
+    }
     $rows = $pdo->query($sql)->fetchAll();
     return $rows;
   } else {
